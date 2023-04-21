21fdfda0e9196077eaf305fc7f5b96ec9a52bf46
JesusCS <jesus.colmser@gmail.com>
2023-04-21 04:41:28 -0600

    ultimos mensajes de chat

--- controllers/user/message/message.js
+++ controllers/user/message/message.js
@@ -1,5 +1,6 @@
 
 const Message = require('../../../models/user/message/message');
+const mongoose = require('mongoose');
 
 /* *** test *** */
 function test (req, res) {
@@ -38,75 +39,6 @@ async function saveMessage (req, res) {
         console.log(error);
         return res.status(500).send({message: 'Error al guardar el mensaje: ' + error});
     }
-
-    /*
-    try {
-        let message = new Message();
-        message.message.text = params.text;
-        message.filesMessage = params.filesMessage;
-        message.comesFrom = params.comesFrom;
-        message.viewed = false; 
-        message.users.emitter = params.emitter;
-        message.users.receiver = params.receiver;
-        message.sender = params.emitter;
-        message.service = params.service;
-
-        let messageStored = await message.save();
-
-        if(!messageStored) return res.status(200).send({message: 'Error al enviar el mensaje'});
-
-        return res.status(200).send({message: 'El mensaje se envió con éxito!'});
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({message: 'Error al guardar el mensaje: ' + error});
-    }
-    */
-
-    /*
-    try {
-        let message = new Message();
-        message.message.text = params.text;
-        message.filesMessage = params.filesMessage;
-        message.comesFrom = params.comesFrom;
-        message.viewed = false; 
-        message.users = [params.emitter, params.receiver];
-        message.sender = params.emitter;
-        message.service = params.service;
-
-        let messageStored = await message.save();
-
-        if(!messageStored) return res.status(200).send({message: 'Error al enviar el mensaje'});
-
-        return res.status(200).send({message: 'El mensaje se envió con éxito!'});
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({message: 'Error al guardar el mensaje: ' + error});
-    }
-    */
-
-    /*
-    const params = req.body;
-
-    if (!params.text || !params.receiver || !params.comesFrom) return res.status(200).send({message: 'Please fill all the fields!'});
-
-    let message = new Message();
-    message.emitter = params.emitter;
-    message.receiver = params.receiver;
-    message.text = params.text;
-    message.filesMessage = params.filesMessage;
-    message.comesFrom = params.comesFrom;
-    message.viewed = false;
-
-    message.save((err, messageStored) => {
-        if (err) return res.status(500).send({message: 'Error in user request'});
-
-        if(!messageStored) return res.status(200).send({message: 'Error to send message'});
-
-        return res.status(200).send({messageSent: messageStored});
-    });
-    */
 }
 
 /* *** get received messages *** */
@@ -136,26 +68,12 @@ function getReceivedMessages (req, res) {
 
 /* *** get chat messages *** */
 async function getChatMessages (req, res) {
-    console.log(req.params);
     let emitterId = req.params.emitter;
     let receiverId = req.params.receiver;
     let serviceId = req.params.service;
     
     try {
         let messages = await Message.find({
-            /*
-            $or: [
-                {emitter: emitterId},
-                {receiver: emitterId},
-            ],
-            */
-            
-           /*
-           $or: [
-                {emitter: emitterId, receiver: receiverId},
-                {emitter: receiverId, receiver: emitterId},
-           ],
-           */
 
            $and: [
                 {service: serviceId},
@@ -170,9 +88,6 @@ async function getChatMessages (req, res) {
            ]
         }).sort({updatedAt: 1});
 
-        console.log(messages);
-        console.log(messages.length);
-
         if(messages.length > 0) {
             let chatMessages = messages.map(message => {
                 return {
@@ -183,40 +98,6 @@ async function getChatMessages (req, res) {
             console.log(chatMessages);
             return res.status(200).send(chatMessages);
         }
-
-        /*
-        let messages = await Message.find({
-            users: {
-                $all: [emitter, receiver],
-            }
-        }).sort({updatedAt: 1});
-
-        console.log(messages);
-
-        let chatMessages = messages.map(message => {
-            return {
-                fromSelf: message.sender.toString() === emitter,
-                message: message.message.text,
-            };
-        });
-        console.log(chatMessages);
-        return res.status(200).send(chatMessages);
-        */
-
-        /*
-        let chatMsgs = await Message.find({emitter: userId, receiver: receiverId}).populate('emitter receiver', '_id username');
-
-        let chatMsgsRe = await Message.find({emitter: receiverId, receiver: userId}).populate('emitter receiver', '_id username');
-        
-        if(!chatMsgs) return res.status(404).send({message: 'El chat no existe'});
-
-        return res.status(200).send(
-          { 
-            chatMessages: chatMsgs,
-            chatMessagesRe: chatMsgsRe,
-          }
-        );
-        */
     } catch (error) {
         console.log(error);
         return res.status(500).send({message: 'Error al devolver el chat: ' + error});
@@ -225,226 +106,122 @@ async function getChatMessages (req, res) {
     
 }
 
-/* */ 
+/* *** get last messages *** */
 async function getLastMessages(req, res) {
-    console.log(req.params);
 
     let userId = req.params.id;
     let comesFrom = req.params.comesFrom;
 
-    //await getChatMsgs(userId, comesFrom);
-
-    const lastChatMessages = await Message.aggregate(
-        [
-            {
-              $match:
-                /**
-                 * query: The query in MQL.
-                 */
+    try {
+        const lastMsgs = await Message.aggregate(
+            [
                 {
-                  $and: [
-                    {
-                      comesFrom: comesFrom,
-                    },
-                    {
-                      $or: [
-                        {
-                          $and: [
+                    $match: {
+                        $and: [
+                            { comesFrom: comesFrom },
                             {
-                              emitter: userId,
+                                $or: [
+                                    {
+                                        $and: [
+                                            {
+                                            emitter: mongoose.Types.ObjectId(userId),
+                                            },
+                                        ],
+                                    },
+                                    {
+                                        $and: [
+                                            {
+                                            receiver: mongoose.Types.ObjectId(userId),
+                                            },
+                                        ],
+                                    },
+                                ],
                             },
-                          ],
-                        },
-                        {
-                          $and: [
-                            {
-                              receiver: userId,
-                            },
-                          ],
-                        },
-                      ],
-                    },
-                  ],
-                },
-            },
-            {
-              $addFields:
-                /**
-                 * newField: The new field name.
-                 * expression: The new field expression.
-                 */
-                {
-                  fromTo: {
-                    $cond: {
-                      if: {
-                        $lt: ["$emitter", "$receiver"],
-                      },
-                      then: ["$emitter", "$receiver"],
-                      else: ["$receiver", "$emitter"],
-                    },
-                  },
-                },
-            },
-            {
-              $group:
-                /**
-                 * _id: The id of the group.
-                 * fieldN: The first field name.
-                 */
-                {
-                  _id: "$fromTo",
-                  messages: {
-                    $addToSet: {
-                      message: "$text",
-                      addAt: "$updatedAt",
-                    },
-                  },
-                  lastMessage: {
-                    $last: "$updatedAt",
-                  },
-                },
-            },
-            {
-              $addFields:
-                /**
-                 * newField: The new field name.
-                 * expression: The new field expression.
-                 */
-                {
-                  lastFilteredMessage: {
-                    $arrayElemAt: [
-                      {
-                        $filter: {
-                          input: "$messages",
-                          as: "msgs",
-                          cond: {
-                            $eq: [
-                              "$$msgs.addAt",
-                              "$lastMessage",
-                            ],
-                          },
-                          //limit: 1,
-                        },
-                      },
-                      0,
-                    ],
-                  },
-                },
-            },
-        ]
-    );
-
-    return res.status(200).json({
-        data: {
-            lastChatMessages
-        }
-    });
-}
-
-async function getChatMsgs (userId, comesFrom){
-    const pipeline = [
-        {
-          $match:
-            /**
-             * query: The query in MQL.
-             */
-            {
-              $and: [
-                {
-                  comesFrom: comesFrom,
-                },
-                {
-                  $or: [
-                    {
-                      $and: [
-                        {
-                          emitter: userId,
-                        },
-                      ],
-                    },
-                    {
-                      $and: [
-                        {
-                          receiver: userId,
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-        },
-        {
-          $addFields:
-            /**
-             * newField: The new field name.
-             * expression: The new field expression.
-             */
-            {
-              fromTo: {
-                $cond: {
-                  if: {
-                    $lt: ["$emitter", "$receiver"],
-                  },
-                  then: ["$emitter", "$receiver"],
-                  else: ["$receiver", "$emitter"],
-                },
-              },
-            },
-        },
-        {
-          $group:
-            /**
-             * _id: The id of the group.
-             * fieldN: The first field name.
-             */
-            {
-              _id: "$fromTo",
-              messages: {
-                $addToSet: {
-                  message: "$text",
-                  addAt: "$updatedAt",
-                },
-              },
-              lastMessage: {
-                $last: "$updatedAt",
-              },
-            },
-        },
-        {
-          $addFields:
-            /**
-             * newField: The new field name.
-             * expression: The new field expression.
-             */
-            {
-              lastFilteredMessage: {
-                $arrayElemAt: [
-                  {
-                    $filter: {
-                      input: "$messages",
-                      as: "msgs",
-                      cond: {
-                        $eq: [
-                          "$$msgs.addAt",
-                          "$lastMessage",
                         ],
-                      },
-                      //limit: 1,
                     },
-                  },
-                  0,
-                ],
-              },
-            },
-        },
-    ];
-
-    let lastChatMessages = await Message.aggregate(pipeline);
-    console.log(lastChatMessages);
-    await lastChatMessages.forEach(message => {
-        console.log(`${message}`);
-    });
+                },
+                {
+                    $addFields: {
+                        fromTo: {
+                            $cond: {
+                                if: {
+                                    $lt: ["$emitter", "$receiver"],
+                                },
+                                then: ["$emitter", "$receiver"],
+                                else: ["$receiver", "$emitter"],
+                            },
+                        },
+                    },
+                },
+                {
+                    $group: {
+                        _id: "$fromTo",
+                        messages: {
+                            $addToSet: {
+                                emitter: "$emitter",
+                                receiver: "$receiver",
+                                message: "$text",
+                                service: "$service",
+                                addAt: "$updatedAt",
+                            },
+                        },
+                        lastMessage: {
+                            $last: "$updatedAt",
+                        },
+                    },
+                },
+                {
+                    $lookup:
+                      {
+                        from: "users",
+                        localField: "_id",
+                        foreignField: "_id",
+                        pipeline: [
+                            { $project: { _id: 1, username: 1, image: 1 } },
+                        ],
+                        as: "usersChat"
+                      }
+                },
+                {
+                    $addFields: {
+                        lastMsg: {
+                            $arrayElemAt: [
+                                {
+                                    $filter: {
+                                        input: "$messages",
+                                        as: "msgs",
+                                        cond: {
+                                            $eq: [
+                                            "$$msgs.addAt",
+                                            "$lastMessage",
+                                            ],
+                                        },
+                                        //limit: 1,
+                                    },
+                                },
+                                0,
+                            ],
+                        },
+                    },
+                },
+                { 
+                    $project: { 
+                        _id: 0,
+                        usersChat: 1, 
+                        lastMsg: 1, 
+                    } 
+                },
+            ]
+        );
+    
+        return res.status(200).send({
+            count: lastMsgs.length,
+            lastMsgs,
+        });        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: 'Error al devolver los ultimos mensajes de chat: ' + error});
+    }
 }
 
 /* *** get emit messages *** */
@@ -473,29 +250,39 @@ function getSentMessages (req, res) {
 }
 
 /* *** unviewed messages *** */
-function unviewedMessages (req, res){
+async function unviewedMessages (req, res){
     let userId = req.user.sub;
 
-    Message.count({receiver: userId, viewed: false}).exec((err, count) => {
-        if (err) return res.status(500).send({message: 'Error in user request'});
+    try {
+        let unviewed = await Message.count({receiver: userId, viewed: false});
+
+        if(!unviewed) return res.status(404).send({message: 'Has visto todos los mensajes!'});
 
         return res.status(200).send({
-            'unviewed': count
+            unviewed: unviewed,
         });
-    });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: 'Error al devolver mensajes no vistos: ' + error});
+    }
 }
 
 /* *** set viewed messages *** */
-function setViewedMessages (req, res) {
+async function setViewedMessages (req, res) {
     let userId = req.user.sub;
 
-    Message.update({receiver: userId, viewed: false}, {viewed: true}, {"multi": true}, (err, messagesUpdated) => {
-        if (err) return res.status(500).send({message: 'Error in user request'});
+    try {
+        let viewed = await Message.updateMany({receiver: userId, viewed: false}, {viewed: true}, {"multi": true});
+
+        if(!viewed) return res.status(404).send({message: 'Has visto todos los mensajes!'});
 
         return res.status(200).send({
-            messages: messagesUpdated
+            viewed: viewed,
         });
-    });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: 'Error en la solicitud: ' + error});
+    }
 }
 
 module.exports = {
